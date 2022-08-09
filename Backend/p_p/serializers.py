86ba7c96d40d02b8dc7c80365020424a1e4017f9
0laolu <abdullateefolaolu@gmail.com>
2022-08-09 09:15:21 +0100

    contains initial code for the mobile view of the landing page

--- Backend/p_p/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import PrivacyPolicy
-
-
-class GetPrivacyPolicySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PrivacyPolicy
-        fields = '__all__'
