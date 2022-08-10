910d37020180ea6ec3b47861da4a0440ad565690
0laolu <abdullateefolaolu@gmail.com>
2022-08-10 08:02:29 +0100

    style the menu bar and add overlay effect to slider

--- JS/script.js
+++ JS/script.js
@@ -17,10 +17,18 @@ function displayAnswer() {
 
 const hamburgerIcon = document.querySelector('.hamburger__wrapper')
 const menuBar = document.querySelector('.mobile-nav__container')
+const menuBarOverlay = document.querySelector('#overlay')
 
 hamburgerIcon.onclick = showMenuBar
 
 function showMenuBar() {
     hamburgerIcon.classList.toggle('active')
     menuBar.classList.toggle('slide-out')
+    menuBarOverlay.classList.toggle('active')
 }
+
+menuBarOverlay.addEventListener('click', function() {
+    hamburgerIcon.classList.remove('active')
+    menuBar.classList.remove('slide-out')
+    menuBarOverlay.classList.remove('active')
+})
